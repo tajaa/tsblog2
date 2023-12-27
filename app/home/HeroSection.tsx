c0f7b126ae43ceeae1432b9223cea8718c86fa04
tajaa <tajatheprince@gmail.com>
2023-12-27 15:15:17 -0800

    added some stuff to herosection

--- app/home/HeroSection.tsx
+++ app/home/HeroSection.tsx
@@ -5,13 +5,10 @@ type Props = {};
 
 const HeroSection = (props: Props) => {
   return (
-    <section>
-      <Image
-        src="/coffee.png"
-        width={300}
-        height={300}
-        alt="person-walking"
-      ></Image>
+    <section className="grid grid-cols-1 text-white">
+      <h1 className="text-2xl sm:text-4xl"> CS News For Busy Coders</h1>
+      <button>Brows Articles</button>
+      <Image src="/coffee.png" width={300} height={300} alt="person-walking" />
     </section>
   );
 };
