89a16687cb666dcff535a56084a98e3df1d9d7ff
tajaa <tajatheprince@gmail.com>
2023-12-29 21:43:51 -0800

    added to the PostCard mapping in posts.tsx to grab the posts from the posgres db

--- app/home/PostCard.tsx
+++ app/home/PostCard.tsx
@@ -3,13 +3,16 @@ import type { Post } from "@prisma/client";
 
 type Props = {
   post: Post;
+  className?: string;
 };
 
-const PostCard = ({ post }: Props) => {
+const PostCard = ({ post, className }: Props) => {
   return (
-    <div>
-      <h3>{post.title}</h3>
-      <p>{post.content}</p>
+    <div
+      className={`${className} p-4 rounded border-2 mt-3 shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)]`}
+    >
+      <h3 className="text-2xl text-black font-serif mb-2 mt-4">{post.title}</h3>
+      <p className="">{post.content}</p>
     </div>
   );
 };
--- app/home/Posts.tsx
+++ app/home/Posts.tsx
@@ -5,17 +5,18 @@ import PostCard from "./PostCard";
 type Props = {};
 
 const Posts = async (props: Props) => {
-  const users = await prisma.user.findMany();
-  //console.log(users);
   const posts = await prisma.post.findMany();
   //console.log(posts);
+  //posts in an array 'cause we have find many
+  //
+  const bgClasses = ["bg-pink-500", "bg-blue-500", "bg-yellow-500"];
 
   return (
     <div>
       <h2 className="text-4xl text-center mt-6">Trending</h2>
-      <div>
-        {posts.map((post) => (
-          <PostCard key={post.id} post={post} />
+      <div className="grid grid-cols-3 gap-4">
+        {posts.map((post, index) => (
+          <PostCard key={post.id} post={post} className={bgClasses[index]} />
         ))}
       </div>
     </div>
