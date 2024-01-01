db626edcdd6dc5aa90ef75da50d35e941cf28aba
tajaa <tajatheprince@gmail.com>
2024-01-01 13:50:49 -0800

    added some layout stuff; working on the auth

---
+++ app/api/[...nextauth]/routes.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth/next";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_URLSECRET as string,
+  debug: true,
+});
