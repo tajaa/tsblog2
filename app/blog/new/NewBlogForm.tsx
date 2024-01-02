95c16d80fdfc05024d18d00c0a69c83d283173c0
tajaa <tajatheprince@gmail.com>
2024-01-01 21:33:54 -0800

    styled the newblogform page

--- app/blog/new/NewBlogForm.tsx
+++ app/blog/new/NewBlogForm.tsx
@@ -30,20 +30,32 @@ const NewBlogForm = (props: Props) => {
   if (submitted) return <div>Post Submitted!</div>;
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="min-h[calc(100vh-130px)] py-2 container flex flex-col mt-12">
+      <form
+        className="flex flex-col items-stretch justify-center h-full text-left"
+        onSubmit={handleSubmit}
+      >
         <input
-          type="name"
+          type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           name="title"
+          placeholder="Title"
+          className="text-6xl focus-visible:outline-none bg-teal-600 text-white font-mono"
         />
         <textarea
           name="content"
           value={content}
           onChange={(e) => setContent(e.target.value)}
+          className="flex-1 text-xl mt-2 bg-black text-white px-1 py-1 rounded-md font-mono"
+          placeholder="ideas here..."
         />
-        <button type="submit">Create</button>
+        <button
+          type="submit"
+          className="w-fit-content bg-teal-300 text-black font-mono py-1 px-4 border-2 rounded mt-2 shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)]"
+        >
+          Create
+        </button>
       </form>
     </div>
   );
