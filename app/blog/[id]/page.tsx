c1d1ee2d44d4ba2a5bd45f840bd3cf64afc7f78f
tajaa <tajatheprince@gmail.com>
2024-01-02 14:40:50 -0800

    fixed styles

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -12,15 +12,27 @@ const page = async ({
     where: {
       id: Number(params.id),
     },
+    include: {
+      author: true,
+    },
   });
   console.log(post);
 
   return (
-    <div>
+    <div className="container mt-6">
       {post && (
         <>
-          <h1>{post.title}</h1>
-          <p>{post.content}</p>
+          <h1 className="text-4xl font-mono mb-5">{post.title}</h1>
+          <div className="flex items-center">
+            <p className="text-sm font-mono text-amber-500">
+              By {post.author.name}
+            </p>
+            <div
+              className="w-20 h-20 rounded-full bg-contain m-2"
+              style={{ backgroundImage: `url(${post.author.image})` }}
+            ></div>
+          </div>
+          <p className="font-mono text-black">{post.content}</p>
         </>
       )}
     </div>
