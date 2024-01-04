36c65efce90c226d9faafdffe445448cb90843a3
tajaa <tajatheprince@gmail.com>
2024-01-03 22:23:55 -0800

    fixed some styling on remove selection

--- app/blog/all/CategorySelector.tsx
+++ app/blog/all/CategorySelector.tsx
@@ -12,18 +12,20 @@ type CategorySelectorPropsWithState = CategorySelectorProps & {
 export const CategorySelector = (props: CategorySelectorPropsWithState) => {
   const bgClasses = ["bg-slate-400", "bg-slate-600", "bg-slate-800"];
   return (
-    <div className="flex flex-wrap flex-col justify-center my-4">
-      {props.categories.map((category, index) => (
-        <button
-          key={category.id}
-          onClick={() => props.selectCategory(category.id)}
-          className={`inline-block font-mono rounded-full px-4 py-2 text-sm text-white mr-4 mb-4 ${
-            bgClasses[index]
-          } ${props.selectedCategoryId === category.id ? "neo-shadow" : ""}`}
-        >
-          {category.name}
-        </button>
-      ))}
+    <div className="flex flex-col justify-center my-4">
+      <div className="flex justify-center flex-wrap">
+        {props.categories.map((category, index) => (
+          <button
+            key={category.id}
+            onClick={() => props.selectCategory(category.id)}
+            className={`inline-block font-mono rounded-full px-4 py-2 text-sm text-white mr-4 mb-4 ${
+              bgClasses[index]
+            } ${props.selectedCategoryId === category.id ? "neo-shadow" : ""}`}
+          >
+            {category.name}
+          </button>
+        ))}
+      </div>
       {props.selectedCategoryId ? (
         <button
           key="remove selection"
