24244d1d5595aede5a14202ec5ae8210c7088df5
tajaa <tajatheprince@gmail.com>
2024-01-03 18:24:40 -0800

    addded the category selection in allarticles

--- app/blog/all/CategorySelector.tsx
+++ app/blog/all/CategorySelector.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { Category } from "@prisma/client";
 
 export type CategorySelectorProps = {
@@ -6,37 +5,22 @@ export type CategorySelectorProps = {
 };
 
 type CategorySelectorPropsWithState = CategorySelectorProps & {
-  selectCategory: (id: number | null) => void;
+  selectCategory: (id: number) => void;
   selectedCategoryId: number | null;
 };
 
-export const CategorySelector = (props: CategorySelectorPropsWithState) => {
-  const bgClasses = ["bg-pink-500", "bg-blue-500", "bg-yellow-500"];
-
+export const CategorySelector = (props: CategorySelectorProps) => {
+  const bgClasses = ["bg-slate-400", "bg-slate-600", "bg-slate-800"];
   return (
-    <div className="flex  flex-col justify-center my-4">
-      <div className="flex justify-center flex-wrap">
-        {props.categories.map((category, index) => (
-          <button
-            key={category.id}
-            onClick={() => props.selectCategory(category.id)}
-            className={`inline-block rounded-full text-xl px-4 py-2 text-white mr-4 mb-4 ${
-              bgClasses[index]
-            } ${props.selectedCategoryId === category.id ? "neo-shadow" : ""}`}
-          >
-            {category.name}
-          </button>
-        ))}
-      </div>
-      {props.selectedCategoryId ? (
-        <button
-          className="cursor-pointer underline"
-          key="remove selection"
-          onClick={() => props.selectCategory(null)}
+    <div className="flex flex-wrap justify-center my-4">
+      {props.categories.map((category, index) => (
+        <div
+          key={category.id}
+          className={`inline-block font-mono rounded-full px-4 py-2 text-sm text-white mr-4 mb-4 ${bgClasses[index]}`}
         >
-          Remove Selection
-        </button>
-      ) : null}
+          {category.name}
+        </div>
+      ))}
     </div>
   );
 };
